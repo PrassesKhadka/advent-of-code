a482b2a9198504acac52760897380cb3b6b655d7
Prasses Khadka <khadkaprasses@gmail.com>
2024-12-06 23:43:40 +0545

    refactor

--- 2024/day2/solution.py
+++ 2024/day2/solution.py
@@ -1,38 +1,45 @@
-import sys
 import os
-
-args=sys.argv
 
 with open(os.path.join("input.txt")) as f:
     input=list(map(lambda x:list(map(int,x.split())),f.readlines()))
 
-safe=[]
-unsafe=[]
-
 def check(arr):
     last=arr[0]
-    flag = "inc" if arr[1]>arr[0] else "dec"
-    diff=arr[1]-arr[0] if flag=="inc" else arr[0]-arr[1]
-    
-    if(diff==0 or diff>3): return False
+    inc = arr[1]>arr[0]
 
     for index,num in enumerate(arr):
-        if(index==0):continue
-
-        if((flag=="inc" and num-last>0 and num-last<=3) or (flag=="dec" and last-num>0 and last-num<=3)):
+        if index==0: continue
+        if((inc and num-last>0 and num-last<=3) or (not inc and last-num>0 and last-num<=3)):
             last=num
             continue
         else:
-            return False
-    return True
+            return index,False
+    return -1,True
 
-def filter(arr):
-    if(check(arr)):
-        safe.append(arr)
-    else:
-        unsafe.append(arr)
+def part1():
+    safe=[]
+    unsafe=[]
 
-for i in input:
-    filter(i)
+    for arr in input:   
+        _,isSafe=check(arr)
+        (safe if isSafe else unsafe).append(arr)
 
-print(f"{len(safe)} reports are safe.")
+    print(f"{len(safe)} reports are safe.")
+
+def part2():
+    safe=[]
+    unsafe=[]
+
+    for arr in input:
+        index,isSafe=check(arr)
+
+        if not isSafe: 
+            arr.pop(index)
+            _,isSafe=check(arr) 
+        
+        (safe if isSafe else unsafe).append(arr)
+    
+    print(f"{len(safe)} reports are safe.")
+
+if __name__=="__main__":
+    part2()
